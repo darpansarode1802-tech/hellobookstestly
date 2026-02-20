b91072a6b6957de57ca0b757242733dc8ebba56a
darpansarode1802-tech <darpansarode1802@gmail.com>
2026-02-20 11:13:23 +0530

    Update login.spec.ts
    new coded is added to the envi

--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -1,7 +1,23 @@
 import { test, expect } from '@playwright/test';
-import { login } from '../../utils/auth';
 
-test('tests/auth/login.spec.ts', async ({ page }) => {
-  await login(page);
+test('Verify Login Functionality - Hellobooks', async ({ page }) => {
+
+  // Navigate to Login Page
+  await page.goto('https://beta.hellobooks.ai/login');
+
+  // Enter Username (Email)
+  await page.fill('#input_your_working_email', 'darpansarode@gmail.com');
+
+  // Enter Password
+  await page.fill('#input_password', 'Darpan@18');
+
+  // Click Sign In Button
+  await page.click("//button//*[text()='Sign In']");
+
+  // Wait for Dashboard URL
+  await page.waitForURL('**/dashboard');
+
+  // Verify URL contains 'dashboard'
   await expect(page).toHaveURL(/dashboard/);
+
 });
